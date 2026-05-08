5f412ce313c766026c2e88edb0688bbc5a2aba56
thestackersudo <reset39594765@gmail.com>
2026-05-08 09:45:13 +0100

    Added a force wait till game loaded

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+repeat task.wait() until game:IsLoaded() end
+
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
