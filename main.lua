73594914c5cb25f165154551b99eff4c5dc06e8a
thestackersudo <reset39594765@gmail.com>
2026-05-15 16:25:19 +0100

    version fix

--- main.lua
+++ main.lua
@@ -20,7 +20,7 @@ localPlayer.Idled:Connect(function()
 end)
 
 local Window = Fluent:CreateWindow({
-    Title = "Plink Slime RNG v1.0.31",
+    Title = "Plink Slime RNG v1.0.33",
     SubTitle = "by who?",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
